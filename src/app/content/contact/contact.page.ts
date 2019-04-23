aa741d731fb247f8cc9571d45f27481f73571132
Whatdapep <pomming1997@gmail.com>
2019-04-23 23:03:54 +0700

    Add Send Email

--- src/app/content/contact/contact.page.ts
+++ src/app/content/contact/contact.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor( private iab: InAppBrowser) { }
+  facebook(){
+ this.iab.create(`https://facebook.com/สหกรณ์ออมทรัพย์กองทัพภาคที่-3-จำกัด-202554849848894/?__tn__=%2Cd%2CP-R&eid=ARCSMscN_RIB9WdqGC4AlaSzhoEgKfKyCZ-8amudac6a_XdA3t9Jo69Gmx1SgRQ8UgZlXg38Xd5yq97n&_rdc=1&_rdr`,`_system`);
+}  
+ngOnInit() {
   }
 
 }
